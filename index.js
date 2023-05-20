eb52d2360093a00ccf2ca1f572016d36b9919f14
rajendra-singh <rajendrasinghdevra332@gmail.com>
2023-05-20 14:20:29 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var i = 0;
+var txt = 'my name is rajendra singh devra i am from udaipur.'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+typeWriter();
